51175d41f5ccf43e53287ff74b6e53bfc79b0c2e
Jean Carlos <jean.berrocal@teaminc.com>
2020-02-19 22:07:01 -0600

    POC

---
+++ TestReactiveUI/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Reactive.Disposables;
+using ReactiveUI;
+using ReactiveUI.XamForms;
+
+namespace TestReactiveUI
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ReactiveContentPage<MainPageViewModel>
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new MainPageViewModel();
+            this.WhenActivated(
+               disposable =>
+               {
+                   this
+                       .Bind(ViewModel, vm => vm.FirstValue, v => v.FirstEntry.Text)
+                       .DisposeWith(disposable);
+                   this
+                      .Bind(ViewModel, vm => vm.SecondValue, v => v.SecondEntry.Text)
+                      .DisposeWith(disposable);
+                   this
+                      .OneWayBind(ViewModel, vm => vm.Result, v => v.ResultEntry.Text)
+                      .DisposeWith(disposable);
+               });
+        }
+    }
+}
---
+++ TestReactiveUI/MainPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ReactiveUI;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace TestReactiveUI
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        private int firstValue;
+        public int FirstValue
+        {
+            get => firstValue;
+            set => this.RaiseAndSetIfChanged(ref firstValue, value);
+        }
+
+        private int secondValue;
+        public int SecondValue
+        {
+            get => secondValue;
+            set => this.RaiseAndSetIfChanged(ref secondValue, value);
+        }
+
+        private int result;
+        public int Result
+        {
+            get => result;
+            set => this.RaiseAndSetIfChanged(ref result, value);
+        }
+
+        public MainPageViewModel()
+        {
+            this.WhenAnyValue(x => x.SecondValue, x => x.FirstValue, (x, y) => x + y).
+                 Subscribe(x => { result = x; });
+        }
+    }
+}
